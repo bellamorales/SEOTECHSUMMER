886dcbd83bef80e293ce41aa5dd3b82619a2d1ff
Marysabel Morales <marysabel.m31@gmail.com>
2022-07-05 02:21:36 +0000

    commit message

--- unitTesting.py
+++ unitTesting.py
@@ -4,11 +4,13 @@ from filename import get_date, get_tweets
 
 class UnitTesting(unittest.TestCase):
     def test_get_date(self):
-        #tests with date 2022-07-03 is entered 
+        # tests with date 2022-07-03 is entered
         self.assertEqual(str(get_date()), "2022-07-03")
+
     def test_get_tweets(self):
         self.assertNotEqual(get_tweets(), None)
         self.assertTrue(len(get_tweets()) != 0)
+
 
 if __name__ == '__main__':
     unittest.main()
